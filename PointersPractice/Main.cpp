cb76b7c3a1d460d4b3be45ac9a341e445a5690e7
Tanner Bebb <17tbebb@gmail.com>
2024-04-01 09:13:52 -0700

    Printing out the variable and the variable address.

--- PointersPractice/Main.cpp
+++ PointersPractice/Main.cpp
@@ -1,7 +1,16 @@
+/*
+This is just a program that I am using to understand strings more.
+*/
+
 #include <iostream>
 using namespace std;
 
 int main() {
+
+	string food = "Pizza"; // A food variable of type string
+
+	cout << food << endl;  // Outputs the value of food (Pizza)
+	cout << &food << endl; // Outputs the memory address of food (0x6dfed4)
 
 	return 0;
 
