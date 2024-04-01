b555db63c30ece3683484078d48d015158cc9703
Tanner Bebb <17tbebb@gmail.com>
2024-04-01 09:22:20 -0700

    Added line 17, I dereferanced the pointer and printed it to the screen. This printed out the value of x rather than printing out the memory address location.

--- PointersPractice/Main.cpp
+++ PointersPractice/Main.cpp
@@ -9,10 +9,12 @@ using namespace std;
 int main() {
 
 	int x = 10;				//X = 10
-	int* ptr = &x;			//Pointer that is set to the memory address location of x.
+	int *ptr = &x;			//Pointer that is set to the memory address location of x.
 
 	cout << x << endl;		//Print out the value stored in x
-	cout << ptr << endl;	//Print out the pointer that is pointing to the memory address location of x. 
+	cout << ptr << endl;	//Print out the pointer that is pointing to the memory address location of x.
+
+	cout << *ptr << endl;	//This dereferances the pointer, so this will print out the value of x instead of the memory address location.
 
 	return 0;
 
